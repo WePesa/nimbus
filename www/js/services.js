b2a8ce51ed492ccbe756a2e5701f07088721301d
kejace <kejace@gmail.com>
2016-02-21 22:51:18 +0100

    an extra 'http://' removed

--- www/js/services.js
+++ www/js/services.js
@@ -440,7 +440,7 @@ var config = angular.module('config', [])
     strato :          "http://strato-dev2.blockapps.net",
     uri:              "http://strato-dev2.blockapps.net" + "/eth/v1.0",
     keyserver:        "http://localhost:8000",
-    ipfsHost :        "http://blockapps-keymaster.cloudapp.net",//"104.131.53.68", //"http://104.236.65.136",
+    ipfsHost :        "blockapps-keymaster.cloudapp.net",//"104.131.53.68", //"http://104.236.65.136",
     ipfsPort :        "5001",
     ipfsWebPort:      "8080",
     personaRegistry:  "d9ffec038375699cc76528f3b7fa5dd07e4ea4df"
